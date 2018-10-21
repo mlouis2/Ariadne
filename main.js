b9ec19310cd5b2f069b572a2336a590c8c0e4c91
Maddie Louis <mlouis2@lion.lmu.edu>
2018-10-21 13:12:40 -0700

    Files added

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+init() {
+     canvas.width = document.body.clientWidth;
+     canvas.height = document.body.clientHeight;
+}
---
+++ maze.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+class Maze {
+     constructor() {
+
+     }
+}
